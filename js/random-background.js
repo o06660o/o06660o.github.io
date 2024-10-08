feaa37251d380bc9f121fb75022c7c082ec760f6
o06660o <2957531723@qq.com>
2024-10-08 15:42:01 +0800

    Site updated: 2024-10-08 15:42:00

--- js/random-background.js
+++ js/random-background.js
@@ -2,21 +2,6 @@ const backgrounds = [
     "/images/backgrounds/Minecraft1.jpg",
     "/images/backgrounds/Minecraft2.jpg",
     "/images/backgrounds/Minecraft3.jpg",
-    "/images/backgrounds/Minecraft4.jpg",
-    "/images/backgrounds/Minecraft5.jpg",
-    "/images/backgrounds/Minecraft6.jpg",
-    "/images/backgrounds/Minecraft7.jpg",
-    "/images/backgrounds/Minecraft8.jpg",
-    "/images/backgrounds/Minecraft9.jpg",
-    "/images/backgrounds/Minecraft10.jpg",
-    "/images/backgrounds/Minecraft11.jpg",
-    "/images/backgrounds/Minecraft12.jpg",
-    "/images/backgrounds/Minecraft13.jpg",
-    "/images/backgrounds/Minecraft14.jpg",
-    "/images/backgrounds/Minecraft15.jpg",
-    "/images/backgrounds/Minecraft16.jpg",
-    "/images/backgrounds/Minecraft17.jpg",
-    "/images/backgrounds/Minecraft18.jpg",
   ];
   
 // randomly select an image
