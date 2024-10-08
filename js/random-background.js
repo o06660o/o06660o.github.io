1ad4e90f9db0366398bc04af972cf947a8d18fe7
o06660o <2957531723@qq.com>
2024-10-08 17:02:50 +0800

    Site updated: 2024-10-08 17:02:50

--- js/random-background.js
+++ js/random-background.js
@@ -1,7 +1,19 @@
 const backgrounds = [
-    "/images/backgrounds/Minecraft1.jpg",
-    "/images/backgrounds/Minecraft2.jpg",
-    "/images/backgrounds/Minecraft3.jpg",
+    "/images/backgrounds/1.png",
+    "/images/backgrounds/2.png",
+    "/images/backgrounds/3.png",
+    "/images/backgrounds/4.png",
+    "/images/backgrounds/5.png",
+    "/images/backgrounds/6.png", 
+    "/images/backgrounds/7.png",
+    "/images/backgrounds/8.png",
+    "/images/backgrounds/9.png",
+    "/images/backgrounds/10.png",
+    "/images/backgrounds/11.png",
+    "/images/backgrounds/12.png",
+    "/images/backgrounds/13.png",
+    "/images/backgrounds/14.png",
+    "/images/backgrounds/15.png",
   ];
   
 // randomly select an image
@@ -13,14 +25,15 @@ const selected = backgrounds[Math.floor(Math.random() * backgrounds.length)];
 // document.body.style.backgroundAttachment = "fixed";
 // document.body.style.backgroundSize = "cover";
 // document.body.style.backgroundPosition = "50% 50%";
-document.addEventListener('DOMContentLoaded', () => {
+
+document.addEventListener("DOMContentLoaded", () => {
   const img = new Image();
   img.src = selected;
   img.onload = () => {
     document.body.style.backgroundImage = `url(${selected})`;
-    document.body.style.backgroundRepeat = 'no-repeat';
-    document.body.style.backgroundAttachment = 'fixed';
-    document.body.style.backgroundSize = 'cover';
-    document.body.style.backgroundPosition = '50% 50%';
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundAttachment = "fixed";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "50% 50%";
   };
 });
